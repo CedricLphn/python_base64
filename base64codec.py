1d871d96302a3e6e113cce2d7f5f8a0bbadb0682
Gustin Florian <florian.gustin.business@gmail.com>
2019-12-10 13:13:42 +0100

    [ADD] from_string_to_list, from_b64_index_to_decimal, decimal_to_binary

--- base64codec.py
+++ base64codec.py
@@ -3,6 +3,9 @@ from src.functions import *
 
 def main():
     print("Hello World")
+    print(from_string_to_list('ABDC'))
+    print(decimal_to_binary(97))
+    print(from_b64_index_to_decimal('a'))
 
 if __name__ == '__main__':
     main()
