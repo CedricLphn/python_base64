c357f7fa0b96218a6aa903f65cf870dd8da27706
Leprohon <cedric.leprohon@yahoo.fr>
2019-12-10 21:16:04 +0100

    [ADD] Decoding function

--- base64codec.py
+++ base64codec.py
@@ -2,25 +2,27 @@ import argparse
 
 from src.functions import *
 
-
 def main():
     """Main module
 
     checks the arguments passed by the user and adds as an exclusion rule 
-    the simultaneous use of encoding and decoding arguments 
+    the simultaneous use of encoding and decoding arguments
     """
+    
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-d", "--decode", action="store_true")
     group.add_argument("-e", "--encode", action="store_true")
     parser.add_argument("-s")
     args = parser.parse_args()
+
     if(args.decode):
-        print("Decoding", decode(args.s))
+        print("Result: ", decode(args.s))
     elif(args.encode):
         print("Encoding", args.s)
     else:
         print("Nothing to do.")
+
 
 
 if __name__ == '__main__':
--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,5 @@
+dictionnaire = {'A': '000000', 'B': '000001', 'C': '000010', 'D': '000011', 'E': '000100', 'F': '000101', 'G': '000110', 'H': '000111', 'I': '001000', 'J': '001001', 'K': '001010', 'L': '001011', 'M': '001100', 'N': '001101', 'O': '001110', 'P': '001111', 'Q': '010000', 'R': '010001', 'S': '010010', 'T': '010011', 'U': '010100', 'V': '010101', 'W': '010110', 'X': '010111', 'Y': '011000', 'Z': '011001', 'a': '011010', 'b': '011011', 'c': '011100', 'd': '011101', 'e': '011110', 'f': '011111', 'g': '100000', 'h': '100001', 'i': '100010', 'j': '100011', 'k': '100100', 'l': '100101', 'm': '100110', 'n': '100111', 'o': '101000', 'p': '101001', 'q': '101010', 'r': '101011', 's': '101100', 't': '101101', 'u': '101110', 'v': '101111', 'w': '110000', 'x': '110001', 'y': '110010', 'z': '110011', '0': '110100', '1': '110101', '2': '110110', '3': '110111', '4': '111000', '5': '111001', '6': '111010', '7': '111011', '8': '111100', '9': '111101', '+': '111110', '/': '111111'}
+
 def b64_table_index():
     """
     Return : index from table b64
@@ -46,4 +48,49 @@ def decode(string_to_decode):
         Text decoded string
     """
 
-    return chr(5)
+    final = ""
+
+    def str_to_b64_decimal(s):
+        """String to binary base 64
+
+        Args:
+            str : String to decode
+
+        Returns:
+            Binary base 64 array
+        """
+
+        list = []
+
+        for char in s.strip('='):
+            list.append(dictionnaire[char])
+            
+        return list
+
+
+    def convert_6b_to_8b(bits): 
+        """Convert array of 6 bit to 8 bits
+
+        Args:
+            bits : array of bits
+
+        Return:
+            List of 8 bits
+
+        """
+        result = []
+        string = ''.join(bits)
+        list_range = range(0, len(string)-1)
+        old_cut = 0
+        for i in list_range:
+            if i % 8 == 0 and i != 0:
+                result.append(string[old_cut:i])
+                old_cut = i
+        return result
+            
+    listing = convert_6b_to_8b(str_to_b64_decimal(string_to_decode))
+
+    for decimal in listing:
+        final += chr(int(decimal, 2))
+
+    return final
